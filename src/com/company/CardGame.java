d58c79df37de4f5000848e2912e36e0aaf8ebf48
Joshua <6238233@gmail.com>
2020-09-29 18:21:40 +0300

    Final

--- src/com/company/CardGame.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-import java.util.Stack;
-
-public class CardGame {
-    Stack<Card>
-}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,55 @@
 package com.company;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+import java.lang.String;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Deque<Integer> FirstPlayerCard = new ArrayDeque<>();
+        Deque<Integer> SecondPlayerCard = new ArrayDeque<>();
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        for (String part : sc.nextLine().split(" ")) {
+            FirstPlayerCard.offerLast(Integer.parseInt(part));
+        }
+        for (String part : sc.nextLine().split(" ")) {
+            SecondPlayerCard.offerLast(Integer.parseInt(part));
+        }
+
+        while (FirstPlayerCard.peek() != null || SecondPlayerCard.peek() != null) {
+            Integer cardFirstPlayer = FirstPlayerCard.poll();
+            Integer cardSecondPlayer = SecondPlayerCard.poll();
+            count++;
+            if (cardFirstPlayer == 0 && cardSecondPlayer == 9) {
+                FirstPlayerCard.addLast(cardFirstPlayer);
+                FirstPlayerCard.addLast(cardSecondPlayer);
+                break;
+            }
+            if (cardFirstPlayer == 9 && cardSecondPlayer == 0) {
+                SecondPlayerCard.addLast(cardFirstPlayer);
+                SecondPlayerCard.addLast(cardSecondPlayer);
+                break;
+            }
+            if (cardFirstPlayer > cardSecondPlayer) {
+                FirstPlayerCard.addLast(cardFirstPlayer);
+                FirstPlayerCard.addLast(cardSecondPlayer);
+            }
+            else {
+                SecondPlayerCard.addLast(cardFirstPlayer);
+                SecondPlayerCard.addLast(cardSecondPlayer);
+            }
+            if (count == 106) {
+                System.out.println("Draw");
+                System.exit(0);
+            }
+        }
+        if (FirstPlayerCard.peek() == null) {
+            System.out.println("Second " + count);
+        } else {
+            System.out.println("First " + count);
+        }
     }
 }
